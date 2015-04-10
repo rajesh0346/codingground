20a48d668cd6e22f93dec319a9acc158ba3fd383
rajesh0346 <rajesh0541@gmail.com>
2015-04-10 14:56:22 +0530

    Created by Coding Ground Client

---
+++ Prep/Newfile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int coinchange(int S[], int m, int n)
+{
+    if (n==0){
+        return 1;
+    }
+    if (n < 0){
+        return 0;
+    }
+    
+    if (m<0 && n>0){
+        return 0;
+    }
+    return (coinchange(S, m-1, n) + coinchange(S, m, n-S[m]));
+}
+
+int main()
+{
+    int S[] = {1, 2, 3};
+    
+    printf("no of ways %d\n", coinchange(S, 2, 10));
+    
+    return 0;
+}
